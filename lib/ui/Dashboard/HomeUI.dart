a97f07387c146f81b6ea02ef6ba03957f31ab28a
rahul.bharti <rahul.bharti@kreatetechnologies.com>
2024-09-13 17:24:01 +0530

    working on issue

--- lib/ui/Dashboard/HomeUI.dart
+++ lib/ui/Dashboard/HomeUI.dart
@@ -8,48 +8,48 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-// Future<Album> fetchAlbum() async {
-//   final response = await http
-//       .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
 
-//   if (response.statusCode == 200) {
-//     // If the server did return a 200 OK response,
-//     // then parse the JSON.
-//     return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-//   } else {
-//     // If the server did not return a 200 OK response,
-//     // then throw an exception.
-//     throw Exception('Failed to load album');
-//   }
-// }
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
 
-// class Album {
-//   final int userId;
-//   final int id;
-//   final String title;
+class Album {
+  final int userId;
+  final int id;
+  final String title;
 
-//   const Album({
-//     required this.userId,
-//     required this.id,
-//     required this.title,
-//   });
+  const Album({
+    required this.userId,
+    required this.id,
+    required this.title,
+  });
 
-//   factory Album.fromJson(Map<String, dynamic> json) {
-//     return switch (json) {
-//       {
-//         'userId': int userId,
-//         'id': int id,
-//         'title': String title,
-//       } =>
-//         Album(
-//           userId: userId,
-//           id: id,
-//           title: title,
-//         ),
-//       _ => throw const FormatException('Failed to load album.'),
-//     };
-//   }
-// }
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'userId': int userId,
+        'id': int id,
+        'title': String title,
+      } =>
+        Album(
+          userId: userId,
+          id: id,
+          title: title,
+        ),
+      _ => throw const FormatException('Failed to load album.'),
+    };
+  }
+}
 
 // Widget chartToRun() {
 //    LabelLayoutStrategy? xContainerLabelLayoutStrategy;
@@ -92,48 +92,6 @@ import 'package:http/http.dart' as http;
 //   return verticalBarChart;
 // }
 
-Future<Album> fetchAlbum() async {
-  final response = await http
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
-
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
-  } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
-    throw Exception('Failed to load album');
-  }
-}
-
-class Album {
-  final int userId;
-  final int id;
-  final String title;
-
-  const Album({
-    required this.userId,
-    required this.id,
-    required this.title,
-  });
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return switch (json) {
-      {
-        'code': int userId,
-        'id': int id,
-        'title': String title,
-      } =>
-        Album(
-          userId: userId,
-          id: id,
-          title: title,
-        ),
-      _ => throw const FormatException('Failed to load album.'),
-    };
-  }
-}
 
 class HomeUI extends StatefulWidget {
   const HomeUI({super.key});
